b1fd2b97732f1c4876b48ea5aadc095f92b4af71
7rans <transfire@gmail.com>
2010-01-09 15:59:51 -0500

    doc: improved demo comments

--- demo/helper.rb
+++ demo/helper.rb
@@ -1,2 +1,23 @@
-require 'ae/expect'
+#require 'ae/expect'
+
+# try rcov option
+# TODO: add a qed command option to check here
+#require 'rcov'
+
+#$qed_rcov_analyzer = Rcov::CodeCoverageAnalyzer.new
+
+#QED.Before do
+#  $qed_rcov_analyzer.install_hook
+#end
+
+#QED.After do
+#  $qed_rcov_analyzer.remove_hook
+#end
+
+#at_exit do
+#  $qed_rcov_analyzer.remove_hook
+#  $qed_rcov_analyzer.dump_coverage_info([Rcov::TextReport.new]) 
+#  # use Rcov::HTMLCoverage above to generate HTML reports; the formatters admit
+#  # a number of options, listed in rcov's RDoc documentation.
+#end
 
