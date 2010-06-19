f4aa845438d34856abbb11fcac425dc16c2c9b69
7rans <transfire@gmail.com>
2010-06-19 15:39:30 -0400

    adjust access to project metadata from QED code

--- lib/qed.rb
+++ lib/qed.rb
@@ -1,6 +1,24 @@
+require 'yaml'
+
 module QED
-  vers = YAML.load(File.read(File.dirname(__FILE__) + '/qed/version.yml'))
-  VERSION = vers.values_at('major', 'minor', 'patch', 'state', 'build').compact.join('.')
+  DIRECTORY = File.dirname(__FILE__) + '/qed'
+
+  PROFILE = YAML.load(File.new(DIRECTORY + '/profile.yml')) rescue {}
+  PACKAGE = YAML.load(File.new(DIRECTORY + '/package.yml')) rescue {}
+
+  VERSION = PACKAGE.values_at('major','minor','patch','build').compact.join('.')
+
+  #
+  def self.const_missing(name)
+    key = name.to_s.downcase
+    if PACKAGE.key?(key)
+      PACKAGE[key]
+    elsif PROFILE.key?(key)
+      PROFILE[key]
+    else
+      super(name)
+    end
+  end
 end
 
 require 'qed/session'
