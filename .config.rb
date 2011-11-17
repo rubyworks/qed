4f2c539044962c2268ae91dfbcdd26ab7189357c
7rans <transfire@gmail.com>
2011-11-16 19:15:27 -0500

    Remove rdoc profile, we use simplcov now. [admin]

--- .config.rb
+++ .config.rb
@@ -1,5 +1,9 @@
+#
+# Setup QED.
+#
 qed do
 
+  # Just to demonstrate profiles.
   profile :sample do
     puts ("*" * 78)
     puts
@@ -10,31 +14,12 @@ qed do
     end
   end
 
+  # Create coverage report.
   profile :cov do
     require 'simplecov'
     SimpleCov.start do
       coverage_dir 'log/coverage'
       #add_group "Label", "lib/qed/directory"
-    end
-  end
-
-  profile :rcov do
-    require 'rcov';
-    #require 'rcov/report'
-
-    $qed_rcov_analyzer = Rcov::CodeCoverageAnalyzer.new
-
-    at_exit do
-      $qed_rcov_analyzer.remove_hook;
-      $qed_rcov_analyzer.dump_coverage_info([Rcov::TextReport.new]);
-      Rcov::HTMLCoverage.new(
-        :color => true,
-        :fsr => 30,
-        :destdir => "log/rcov",
-        :callsites => false,
-        :cross_references => false,
-        :charset => nil 
-      ).execute
     end
   end
 
