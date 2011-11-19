da30807e400266110aa51e228cb13fddb83aba14
7rans <transfire@gmail.com>
2011-11-19 13:37:55 -0500

    YARD plugin will be a separate project.

--- work/defunct/yard-qed.rb
+++ work/defunct/yard-qed.rb
@@ -1,1 +1,31 @@
-# TODO: Create a YARD plugin for QED.
+module YARD::CodeObjects
+  class QEDFileObject < ExtraFileObject
+    #
+    def initialize(dirname)
+      self.filename = dirname
+      self.name = File.basename(filename).gsub(/\.[^.]+$/, '').upcase
+      self.attributes = SymbolHash.new(false)
+
+      files = Dir["#{dirname}/**/*{.rdoc,.md,.qed,.markdown}"]
+      files = files.reject{ |f| File.directory?(f) }
+      files = files.sort
+      contents = files.map{ |f| File.read(f) }.join("\n\n")
+
+      parse_contents(contents)
+    end
+  end
+end
+
+module YARD
+  module CLI
+    class Yardoc
+      alias run_without_qed run
+      def run(*args)
+        dir = Dir['{qed/,demo/,spec/}'].first.chomp('/')
+        @options[:files] << CodeObjects::QEDFileObject.new(dir)
+        run_without_qed(*args)
+      end
+    end
+  end
+end
+
