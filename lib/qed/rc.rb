4d2f67b113d49ca788ef212c33238c8c304a9809
7rans <transfire@gmail.com>
2012-05-19 20:14:34 -0400

    Fix RC support (again ugh). [minor]

--- lib/qed/rc.rb
+++ lib/qed/rc.rb
@@ -1,12 +1,12 @@
+require 'qed'
+require 'qed/configure'
+
 begin
-  require 'rc'
-  RC.profile_switch('qed', '-p', '--profile')
+  require 'rc/api'
+  RC.profile_switch 'qed', '-p', '--profile'
   RC.configure 'qed' do |config|
     QED.configure(config.profile, &config)
   end
 rescue LoadError
 end
-
-require 'qed/configure'
-require 'qed'
 
