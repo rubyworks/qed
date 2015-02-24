443d72c593daa3a50ab1428b9e2c5c5b36d15e5b
7rans <transfire@gmail.com>
2015-02-24 12:00:28 -0500

    Port String#tabto from facets.

--- lib/qed/core_ext.rb
+++ lib/qed/core_ext.rb
@@ -43,3 +43,39 @@ class Object
 
 end
 
+class String
+
+  # from facets
+  def tabto(num=nil, opts={})
+    raise ArgumentError, "String#margin has been renamed to #trim." unless num
+
+    tab = opts[:tab] || 2
+    str = gsub("\t", " " * tab)  # TODO: only leading tabs ?
+
+    if opts[:lead]
+      if self =~ /^( *)\S/
+        indent(num - $1.length)
+      else
+        self
+      end
+    else
+      min = []
+      str.each_line do |line|
+        next if line.strip.empty?
+        min << line.index(/\S/)
+      end
+      min = min.min
+      str.indent(num - min)
+    end
+  end
+
+  # from facets
+  def indent(n, c=' ')
+    if n >= 0
+      gsub(/^/, c * n)
+    else
+      gsub(/^#{Regexp.escape(c)}{0,#{-n}}/, "")
+    end
+  end
+
+end
